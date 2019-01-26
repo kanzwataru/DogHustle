81b02f8ddda2b0a76ac009dbe91025344a7c4b4e
Neighto <nathanmladd@gmail.com>
2019-01-25 22:13:38 -0500

    moving and barking dog

---
+++ Assets/GameJamGame/Scripts/Test/MoveTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveTest : MonoBehaviour {
+	public Transform goal;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<NavMeshAgent>().destination = goal.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Motor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Motor : MonoBehaviour {
+
+    private Rigidbody rigidBody;
+
+    public float speed = 30f;
+    public bool canMove = true;
+
+	private void Start () {
+        rigidBody = this.GetComponent<Rigidbody>();
+	}
+
+    public void Move(Vector2 dir)
+    {
+        if (canMove)
+        {
+            rigidBody.AddForce(dir * speed);
+        }
+    }
+
+}
