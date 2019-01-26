663fb3da62fbe6b26b4a62cbd290743045b5201a
神崎航 <kanzwataru@gmail.com>
2019-01-26 10:06:08 -0500

    * fix files messed up by merge conflict markers

--- Assets/GameJamGame/Scripts/Test/MoveTest.cs
+++ Assets/GameJamGame/Scripts/Test/MoveTest.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,23 +16,3 @@ public class MoveTest : MonoBehaviour {
 		
 	}
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.AI;
-
-public class MoveTest : MonoBehaviour {
-	public Transform goal;
-
-	// Use this for initialization
-	void Start () {
-		GetComponent<NavMeshAgent>().destination = goal.position;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-}
->>>>>>> nathan
